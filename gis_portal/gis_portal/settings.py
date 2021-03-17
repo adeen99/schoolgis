2b753013eb2c350c3de35bc0c761eb1deae4ce6f
adeen99 <67963843+adeen99@users.noreply.github.com>
2021-03-18 03:37:14 +0530

    wfs added

--- gis_portal/gis_portal/settings.py
+++ gis_portal/gis_portal/settings.py
@@ -134,6 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'gis_portal/static')]
 
 CRISPY_TEMPALTE_PACK="bootstrap4"
 
